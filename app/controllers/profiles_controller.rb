5be09aaee1c312f20cb3b20bf70129213b22ac45
Sounkoun Soumare <vsoumare@yahoo.com>
2016-08-08 23:33:06 +0000

    Added users show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -19,5 +19,5 @@ class ProfilesController < ApplicationController
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
-  end
+
 end
